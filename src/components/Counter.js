ed96be25676a49c1b36b59e59ffb7bb91f377070
Umashree <umashree@dijisuji.com>
2024-06-12 19:00:18 +0530

    updated counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,10 +6,15 @@ const Counter = () => {
     const IncFnc = ()=>{
         setCount(count+1)
     }
+
+    const DecFnc = ()=>{
+        setCount(count-1)
+    }
   return (
     <>
     <h1>Count: {count}</h1>
     <button type='button' onClick={IncFnc}>Increment</button>
+    <button type='button' onClick={DecFnc}>Decrement</button>
     </>
   )
 }
